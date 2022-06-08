0fa3980cce88c0e4a6b156f3ef652d5373221818
KYJKY <kyj_3052@naver.com>
2022-06-09 02:07:35 +0900

    초기 커밋.

---
+++ SymmetricAlgorithm/DES_Hashing.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace SymmetricAlgorithm
+{
+    internal class DES_Hashing
+    {
+        // 속성
+        public string str { get; private set; }
+        public string CryptKeySting { get; private set; }
+
+        // 생성자
+        public DES_Hashing(string input, string key)
+        {
+            this.str = input;
+            this.CryptKeySting = key;
+        }
+
+        // 암호화
+        public string EncryptInput()
+        {
+            try
+            {
+                // Create a MemoryStream.
+                MemoryStream mStream = new MemoryStream();
+
+                // Create a new DES object.
+                DES DESalg = DES.Create();
+                DESalg.Key = new ASCIIEncoding().GetBytes(CryptKeySting);
+                DESalg.IV = new ASCIIEncoding().GetBytes(CryptKeySting);
+
+                // Create a CryptoStream using the MemoryStream
+                // and the passed key and initialization vector (IV).
+                CryptoStream cStream = new CryptoStream(mStream,
+                    DESalg.CreateEncryptor(DESalg.Key, DESalg.IV),
+                    CryptoStreamMode.Write);
+
+                // Convert the passed string to a byte array.
+                byte[] toEncrypt = new ASCIIEncoding().GetBytes(str);
+
+                // Write the byte array to the crypto stream and flush it.
+                cStream.Write(toEncrypt, 0, toEncrypt.Length);
+                cStream.FlushFinalBlock();
+
+                // Get an array of bytes from the
+                // MemoryStream that holds the
+                // encrypted data.
+                byte[] ret = mStream.ToArray();
+
+                // Close the streams.
+                cStream.Close();
+                mStream.Close();
+
+                // Return the encrypted buffer.
+                return Convert.ToBase64String(ret);
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine("A Cryptographic error occurred: {0}", e.Message);
+                return null;
+            }
+
+        }
+
+        // 복호화
+        public string DecryptInput()
+        {
+            try
+            {
+                byte[] Data = new ASCIIEncoding().GetBytes(str);
+                // Create a new MemoryStream using the passed
+                // array of encrypted data.
+                MemoryStream msDecrypt = new MemoryStream(Data);
+
+                // Create a new DES object.
+                DES DESalg = DES.Create();
+                DESalg.Key = new ASCIIEncoding().GetBytes(CryptKeySting);
+                DESalg.IV = new ASCIIEncoding().GetBytes(CryptKeySting);
+
+                // Create a CryptoStream using the MemoryStream
+                // and the passed key and initialization vector (IV).
+                CryptoStream csDecrypt = new CryptoStream(msDecrypt,
+                    DESalg.CreateDecryptor(DESalg.Key, DESalg.IV),
+                    CryptoStreamMode.Read);
+
+                // Create buffer to hold the decrypted data.
+                byte[] fromEncrypt = new byte[Data.Length];
+
+                // Read the decrypted data out of the crypto stream
+                // and place it into the temporary buffer.
+                csDecrypt.Read(fromEncrypt, 0, fromEncrypt.Length);
+
+                //Convert the buffer into a string and return it.
+                return new ASCIIEncoding().GetString(fromEncrypt);
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine("A Cryptographic error occurred: {0}", e.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ SymmetricAlgorithm/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace SymmetricAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(; ; )
+            {
+                string input = Console.ReadLine();
+
+                // 키를 지정하고 실행
+                string DES_Key = "test";
+
+                DES_Hashing des = new DES_Hashing(input, DES_Key);         
+
+                Console.WriteLine("DES 암호화 결과");
+                Console.WriteLine(des.EncryptInput());
+                Console.WriteLine("DES 복호화 결과");
+                Console.WriteLine(des.DecryptInput());
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
